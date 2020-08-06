344aa8fca9fa31f0034f8d36d42b77c0d553ac9c
Jerry Tsai <purrup27@gmail.com>
2020-08-06 14:21:07 +0800

    set headers)

--- app.js
+++ app.js
@@ -30,7 +30,13 @@ app.use(express.static('dist'))
 app.use(bodyParser.json())
 app.use(passport.initialize())
 app.use(cookieParser())
-
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Credentials', true)
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+  res.header('Access-Control-Allow-Headers', 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json')
+  next()
+})
 db.on('error', () => {
   console.log('failed to connect to mongodb!')
 })
